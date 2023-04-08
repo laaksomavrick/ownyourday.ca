1bdecf43d5045b7c6735ad5a404bc999d1f3f4cf
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-04-08 10:10:33 -0400

    Installed devise and plumbed minimum configuration for devise

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  devise :database_authenticatable,
+         :rememberable,
+         :trackable, :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.from_omniauth(auth)
+    # TODO: if we ever want multiple oauth providers, this logic will have to change to support same email
+    # across multiple providers
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User do
+  # rubocop:disable RSpec/PendingWithoutReason
+  pending "add some examples to (or delete) #{__FILE__}"
+  # rubocop:enable RSpec/PendingWithoutReason
+end
