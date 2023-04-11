09766e047ac7a24116cef5d08239f44c699f91b1
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-04-10 21:19:21 -0400

    Cleaned up login page

---
+++ app/helpers/form_helper.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module FormHelper
+  def label_class
+    'w-full text-md font-medium'
+  end
+
+  def text_field_class
+    'w-full mt-2 outline-none rounded-md border-gray-400'
+  end
+end
