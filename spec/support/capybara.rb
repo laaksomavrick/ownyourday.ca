6bf9f7487362f0d7d4efcac18a6f972739d4288a
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-04-14 15:51:51 -0400

    Configure download path for chrome for CI

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,28 +1,27 @@
 # frozen_string_literal: true
 
-Capybara.register_driver :selenium_chrome_headless do |app|
-  version = Capybara::Selenium::Driver.load_selenium
-  options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
-  browser_options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
-    opts.add_argument('--headless')
-    opts.add_argument('--disable-gpu') if Gem.win_platform?
-    # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
-    opts.add_argument('--disable-site-isolation-trials')
-    opts.add_preference('download.default_directory', Capybara.save_path)
-    opts.add_preference(:download, default_directory: Capybara.save_path)
-  end
+is_ci = ENV.fetch('IS_CI', false)
+Rails.logger.info("is_ci = #{is_ci}")
 
-  client = Selenium::WebDriver::Remote::Http::Default.new
-  client.open_timeout = 120
-  client.read_timeout = 120
-  Capybara::Selenium::Driver.new(app, **{ browser: :chrome, http_client: client, options_key => browser_options })
+Capybara.register_driver :headless_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(
+    args: %w[headless no-sandbox disable-gpu disable-dev-shm-usage],
+    binary: '/usr/bin/google-chrome'
+  )
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    options: options
+  )
 end
 
 RSpec.configure do |config|
-  is_ci = ENV.fetch('IS_CI', false)
   if is_ci
     config.before(:each, type: :system) do
-      driven_by :selenium_chrome_headless
+      Rails.logger.info('Running system test with headless chrome')
+      driven_by :headless_chrome
+      # driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
     end
   end
 end
