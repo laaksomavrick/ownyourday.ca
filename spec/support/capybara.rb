bf232b2c8bf3f69f0dc541ad1c078b73781b9b07
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-04-14 22:08:39 -0400

    Fixup for headless chrome

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 is_ci = ENV.fetch('IS_CI', false)
-Rails.logger.info("is_ci = #{is_ci}")
 
 Capybara.register_driver :headless_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new(
@@ -12,16 +11,14 @@ Capybara.register_driver :headless_chrome do |app|
   Capybara::Selenium::Driver.new(
     app,
     browser: :chrome,
-    options: options
+    options:
   )
 end
 
 RSpec.configure do |config|
   if is_ci
     config.before(:each, type: :system) do
-      Rails.logger.info('Running system test with headless chrome')
       driven_by :headless_chrome
-      # driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
     end
   end
 end
