53180a0c0f220ae3c4e147f71c59b5953bd715ac
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-04-22 20:56:58 -0400

    Update controller and view for new goal modulization

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -2,11 +2,11 @@
 
 class GoalsController < ApplicationController
   def index
-    @goals = policy_scope(Goal)
+    @goals = policy_scope(Goals::Goal)
   end
 
   def show
     goal_id = params[:id].to_i
-    @goal = authorize Goal.find_by(id: goal_id)
+    @goal = authorize Goals::Goal.find_by(id: goal_id)
   end
 end
