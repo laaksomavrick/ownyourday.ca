dd49b56f317cac79e2279fc0d15fa68e2ea8ad7c
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-05-08 09:59:21 -0400

    Extracted UpdateGoalAction; refactoring

---
+++ app/actions/update_goal_action.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+class UpdateGoalAction
+  def initialize(goal)
+    @goal = goal
+  end
+
+  def call(updates = {}, opts = {})
+    persist = opts[:persist] || false
+
+    should_update_type = updates[:type].present?
+
+    @goal = update_type(@goal, updates[:type]) if should_update_type
+
+    should_update_times_per_week = updates[:times_per_week].present? && @goal.is_times_per_week?
+    should_update_days_of_week = updates[:days_of_week].present? && @goal.is_days_of_week?
+
+    @goal.name = updates[:name] if updates[:name]
+    @goal.user_id = updates[:user_id] if updates[:user_id]
+
+    @goal = update_times_per_week(@goal, updates[:times_per_week]) if should_update_times_per_week
+    @goal = update_days_of_week(@goal, updates[:days_of_week]) if should_update_days_of_week
+
+    @goal.save if persist
+
+    @goal
+  end
+
+  private
+
+  def update_type(goal, type)
+    type = type.constantize
+    goal = goal.becomes(type)
+    goal.type = type
+    case goal.type
+    when Goals::Daily.name
+      goal.metadata = '{}'
+    when Goals::TimesPerWeek.name
+      goal.times_per_week = 1
+    when Goals::DaysOfWeek.name
+      goal.days_of_week = [0]
+    else
+      raise "Unknown type when updating goal type=#{goal.type}"
+    end
+
+    goal
+  end
+
+  def update_times_per_week(goal, times_per_week)
+    goal.times_per_week = times_per_week
+    goal
+  end
+
+  def update_days_of_week(goal, days_of_week)
+    days_of_week_keys = days_of_week.keys.map(&:to_s)
+    days_of_week = days_of_week_keys.filter { |day| days_of_week[day] == '1' }
+    goal.days_of_week = days_of_week
+    goal
+  end
+end
--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -7,20 +7,22 @@ class GoalsController < ApplicationController
 
   def new
     @goal = authorize Goals::Daily.new
-    @goal = update_goal_from_form(@goal)
+    update_goal_action = UpdateGoalAction.new(@goal)
+    @goal = update_goal_action.call(goal_params)
   end
 
   def edit
     goal_id = params[:id].to_i
     @goal = authorize Goals::Goal.find_by(id: goal_id)
-    @goal = update_goal_from_form(@goal)
+    update_goal_action = UpdateGoalAction.new(@goal)
+    @goal = update_goal_action.call(goal_params)
   end
 
   def create
     @goal = authorize Goals::Goal.new
-    @goal = update_goal_from_form(@goal)
-    @goal.user = current_user
-    @goal.save
+
+    update_goal_action = UpdateGoalAction.new(@goal)
+    @goal = update_goal_action.call(goal_params, { persist: true })
 
     if @goal.errors.empty? == false
       render 'new', status: :unprocessable_entity
@@ -34,8 +36,9 @@ class GoalsController < ApplicationController
   def update
     goal_id = params[:id].to_i
     @goal = authorize Goals::Goal.find_by(id: goal_id)
-    @goal = update_goal_from_form(@goal)
-    @goal.save
+
+    update_goal_action = UpdateGoalAction.new(@goal)
+    @goal = update_goal_action.call(goal_params, { persist: true })
 
     if @goal.errors.empty? == false
       render 'edit', status: :unprocessable_entity
@@ -48,44 +51,9 @@ class GoalsController < ApplicationController
 
   private
 
-  # TODO: extract this into an action e.g. UpsertGoal(params, { persistence: true })
-  def update_goal_from_form(goal)
-    if goal_params[:type]
-      type = goal_params[:type].constantize
-      goal = goal.becomes(type)
-      goal.type = type
-      case goal.type
-      when Goals::Daily.name
-        goal.metadata = '{}'
-      when Goals::TimesPerWeek.name
-        goal.times_per_week = 1
-      when Goals::DaysOfWeek.name
-        goal.days_of_week = [0]
-      else
-        raise "Unknown type when updating goal type=#{goal.type}"
-      end
-    end
-
-    goal.name = goal_params[:name] if goal_params[:name]
-
-    # goal.user = user if user
-
-    if goal_params[:times_per_week] && goal.is_times_per_week?
-      times_per_week = goal_params[:times_per_week]
-      goal.times_per_week = times_per_week
-    end
-
-    if goal_params[:days_of_week] && goal.is_days_of_week?
-      days_of_week_keys = goal_params[:days_of_week].keys.map(&:to_s)
-      days_of_week = days_of_week_keys.filter { |day| goal_params[:days_of_week][day] == '1' }
-      goal.days_of_week = days_of_week
-    end
-
-    goal
-  end
-
   def goal_params
     params.fetch(:goal, {}).permit(
+      :user_id,
       :name,
       :type,
       :times_per_week,
