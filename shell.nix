e538546f6e152435d1e2e8f760683defe54b5328
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-05-11 21:49:10 -0400

    Updated packages in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
 { pkgs ? import (fetchTarball "http://nixos.org/channels/nixos-22.11/nixexprs.tar.xz") {} }:
 
 pkgs.mkShell {
-  buildInputs = [
+  packages = [
     pkgs.gnumake
-    pkgs.ruby_3_1
     pkgs.docker
+    pkgs.ruby_3_1
     pkgs.nodejs
     pkgs.nodePackages.pnpm
   ];
