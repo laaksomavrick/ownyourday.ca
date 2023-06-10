7e80f7f36fb037f5656d32bc23739bf9b6588379
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-06-10 17:45:32 -0400

    Accommodate position for task list generation

--- app/actions/generate_todays_task_list_action.rb
+++ app/actions/generate_todays_task_list_action.rb
@@ -16,12 +16,11 @@ class GenerateTodaysTaskListAction
       times_per_week_tasks = times_per_week_goals_to_schedule(task_list:, today:)
 
       tasks = daily_tasks + day_of_week_tasks + times_per_week_tasks
+      tasks.sort_by! { |task| task[:position] }
       tasks = tasks.map.with_index do |task, index|
         task[:position] = index
         task
       end
-
-      # TODO: order by goal position
 
       Tasks::GoalTask.create!(tasks)
 
@@ -34,7 +33,7 @@ class GenerateTodaysTaskListAction
   def daily_goals_to_schedule(task_list:)
     daily_goals = @user.daily_goals
     daily_goals.map do |daily_goal|
-      { user: @user, goal: daily_goal, task_list: }
+      { user: @user, goal: daily_goal, task_list:, position: daily_goal.position }
     end
   end
 
@@ -43,7 +42,10 @@ class GenerateTodaysTaskListAction
     days_of_week_goals = @user.days_of_week_goals
     days_of_week_goals
       .filter { |day_of_week_goal| day_of_week_goal.days_of_week.include?(current_day_of_week) }
-      .map { |day_of_week_goal| { user: @user, goal: day_of_week_goal, task_list: } }
+      .map do |day_of_week_goal|
+      { user: @user, goal: day_of_week_goal, task_list:,
+        position: day_of_week_goal.position }
+    end
   end
 
   def times_per_week_goals_to_schedule(task_list:, today:)
@@ -65,7 +67,10 @@ class GenerateTodaysTaskListAction
       .filter do |times_per_week_goal|
         completed_tasks.where(goal_id: times_per_week_goal.id).count < times_per_week_goal.times_per_week
       end
-      .map { |times_per_week_goal| { user: @user, goal: times_per_week_goal, task_list: } }
+      .map do |times_per_week_goal|
+      { user: @user, goal: times_per_week_goal, task_list:,
+        position: times_per_week_goal.position }
+    end
     # rubocop:enable Style/MultilineBlockChain
   end
 end
