51dbe6df19a02134c16f3a7c3d5ff5a1c9545e4f
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-07-31 19:54:45 -0400

    Fix for broken tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,5 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  # rubocop:disable RSpec/PendingWithoutReason
   pending "add some examples to (or delete) #{__FILE__}"
-  # rubocop:enable RSpec/PendingWithoutReason
 end
