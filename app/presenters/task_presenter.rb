de949f708264e8d5201f9ef644c2bbf664583f0f
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-08-14 16:12:27 -0400

    Updated task presenter logic for context and tasks tests

--- app/presenters/task_presenter.rb
+++ app/presenters/task_presenter.rb
@@ -25,8 +25,8 @@ class TaskPresenter
 
     completions_expected = task.goal.times_per_week
 
-    start_of_week = DateTime.current.utc.monday
-    end_of_week = DateTime.current.utc.sunday.beginning_of_day
+    start_of_week = task.task_list.date.monday.beginning_of_day
+    end_of_week = task.task_list.date.sunday.beginning_of_day
     completions_this_week = Tasks::GoalTask
                             .joins(:task_list)
                             .where(goal_id: task.goal.id)
