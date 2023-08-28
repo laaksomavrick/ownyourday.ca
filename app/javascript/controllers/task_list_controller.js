dce751b7a1d1ce90f5dd2fc73e7822f729342a09
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-08-28 18:23:14 -0400

    Added delay and visual indication of drag start to task list

--- app/javascript/controllers/task_list_controller.js
+++ app/javascript/controllers/task_list_controller.js
@@ -5,6 +5,28 @@ export default class extends Sortable {
     super.connect();
   }
 
+  async onChoose(e) {
+    const taskDiv = e.item.childNodes[3].childNodes[1];
+
+    if (!taskDiv) {
+      return;
+    }
+
+    taskDiv.classList.add('border-blue-300');
+    taskDiv.classList.remove('border-gray-300');
+  }
+
+  async onUnchoose(e) {
+    const taskDiv = e.item.childNodes[3].childNodes[1];
+
+    if (!taskDiv) {
+      return;
+    }
+
+    taskDiv.classList.add('border-gray-300');
+    taskDiv.classList.remove('border-blue-300');
+  }
+
   async onUpdate({ item, newIndex }) {
     // https://github.com/stimulus-components/stimulus-sortable/blob/master/src/index.ts
     if (!item.dataset.taskId) return;
@@ -28,6 +50,9 @@ export default class extends Sortable {
   get defaultOptions() {
     return {
       animation: 125,
+      delay: 500,
+      onChoose: this.onChoose,
+      onUnchoose: this.onUnchoose,
     };
   }
 }
