3d27b060fba49fe776da9d193ff6b3f753544baf
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-09-01 11:42:55 -0400

    Fix for importmapping

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,6 +8,8 @@ pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 
+pin 'addReloadToTasksOnActive'
+
 pin 'sortablejs', to: 'https://ga.jspm.io/npm:sortablejs@1.15.0/modular/sortable.esm.js'
 pin 'stimulus-sortable', to: 'https://ga.jspm.io/npm:stimulus-sortable@4.1.0/dist/stimulus-sortable.mjs'
 pin '@rails/request.js', to: 'https://ga.jspm.io/npm:@rails/request.js@0.0.8/src/index.js'
