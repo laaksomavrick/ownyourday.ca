311636e71d38b538cd49792ede0db0553306bef2
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-09-16 16:42:11 -0400

    Added Milestone model validations

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -3,6 +3,7 @@
 class Milestone < ApplicationRecord
   belongs_to :goal, class_name: 'Goals::Goal'
   validates :name, :description, presence: true
+  validate :completed_at, :validate_completed_at
 
   def active?
     completed == true
@@ -16,5 +17,12 @@ class Milestone < ApplicationRecord
     def policy_class
       MilestonePolicy
     end
+  end
+
+  private
+
+  def validate_completed_at
+    is_completed = completed == true
+    errors.add(:completed_at, 'must be set when completed') if is_completed && completed_at.nil?
   end
 end
--- spec/models/milestone_spec.rb
+++ spec/models/milestone_spec.rb
@@ -3,7 +3,26 @@
 require 'rails_helper'
 
 RSpec.describe Milestone do
-  pending 'validates name'
-  pending 'validates description'
-  pending 'validates completed_at when completed is true'
+  it 'can create a milestone' do
+    milestone = create(:milestone)
+    expect(milestone.valid?).to be(true)
+  end
+
+  it 'requires a name' do
+    expect do
+      create(:milestone, name: '')
+    end.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Name can't be blank")
+  end
+
+  it 'requires a description' do
+    expect do
+      create(:milestone, description: '')
+    end.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Description can't be blank")
+  end
+
+  it 'requires completed_at when completed is true' do
+    expect do
+      create(:milestone, completed: true, completed_at: nil)
+    end.to raise_error(ActiveRecord::RecordInvalid, 'Validation failed: Completed at must be set when completed')
+  end
 end
