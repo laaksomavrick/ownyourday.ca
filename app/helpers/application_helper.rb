1284b5f9cb7df1ca869ca94fc59deb91bb2953b9
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-09-21 16:24:22 -0400

    Updated styling for task list screen

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,24 +2,24 @@
 
 module ApplicationHelper
   def brand_color
-    'teal'
+    'indigo'
   end
 
   def primary_button(css = '')
-    "bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded cursor-pointer h-fit #{css}"
+    "bg-indigo-500 hover:bg-indigo-400 text-white font-bold py-2 px-4 rounded w-full shadow hover:shadow-md #{css}"
   end
 
   def primary_button_disabled(css = '')
-    "bg-green-200 text-white font-bold py-2 px-4 rounded cursor-not-allowed h-fit #{css}"
+    "bg-indigo-200 text-white font-bold py-2 px-4 rounded cursor-not-allowed h-fit shadow #{css}"
   end
 
   def destroy_button(css = '')
-    "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded cursor-pointer #{css}"
+    "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded cursor-pointer shadow hover:shadow-md #{css}"
   end
 
   # rubocop:disable Layout/LineLength
   def neutral_button(css = '')
-    "bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-gray-300 hover:border-transparent rounded #{css}"
+    "bg-transparent hover:bg-indigo-500 text-indigo-500 font-semibold hover:text-white py-2 px-4 border border-gray-300 hover:border-transparent rounded shadow hover:shadow-md #{css}"
   end
   # rubocop:enable Layout/LineLength
 
