0350b8d863309d91ab5be0ce88ab0bedd1eea68c
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-09-25 08:54:45 -0400

    Added requests over time

--- terraform/modules/metrics/dashboard.tf
+++ terraform/modules/metrics/dashboard.tf
@@ -2,6 +2,18 @@
 # https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/search-expression-syntax.html
 # https://docs.aws.amazon.com/AmazonCloudWatch/latest/APIReference/CloudWatch-Dashboard-Body-Structure.html#CloudWatch-Dashboard-Properties-Metric-Widget-Object
 
+# TODO:
+# DB cpu, memory, disc
+
+# request latencies
+# 2xx, 4xx, 5xx rates
+# GET, POST, ... counts
+# Payload size (data transfer over time)?
+# Uptime
+
+# Number of Successful deployments
+# Time to deploy
+
 locals {
   five_minutes = 300
   one_day      = 86400
@@ -29,7 +41,7 @@ resource "aws_cloudwatch_dashboard" "main" {
           period  = local.five_minutes
           stacked = false
           region  = data.aws_region.current.name
-          title   = "CPU utilization"
+          title   = "Cluster CPU utilization"
           view    = "timeSeries"
           yAxis = {
             left = {
@@ -54,13 +66,32 @@ resource "aws_cloudwatch_dashboard" "main" {
           period  = local.five_minutes
           stacked = false
           region  = data.aws_region.current.name
-          title   = "Memory utilization"
+          title   = "Cluster memory utilization"
           view    = "timeSeries"
           yAxis = {
             left = {
               showUnits = true
             }
           }
+        }
+      },
+      {
+        type = "metric"
+        x = 6
+        y = 0
+        width = 6
+        height = 6
+        properties = {
+          metrics = [
+            [ "AWS/ApplicationELB", "RequestCountPerTarget", "TargetGroup", "targetgroup/ownyourday-lb-target-group/d980f1d5be1a8460", { visible : false } ],
+            [ ".", "RequestCount", ".", ".", "LoadBalancer", "app/ownyourday-app-load-balancer/bae0e3e1c8bd45fb" ]
+          ],
+          stat = "Sum"
+          period = local.five_minutes
+          stacked = false
+          region = data.aws_region.current.name
+          view = "timeSeries"
+          title   = "Request frequency"
         }
       }
     ]
