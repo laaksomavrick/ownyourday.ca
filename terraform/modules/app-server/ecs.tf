5652aac608647a12e45c95a1b462dbe2b9d4e95c
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-10-09 14:16:41 -0400

    Updating CPU and rebuilding image to 1.8.0

--- terraform/modules/app-server/ecs.tf
+++ terraform/modules/app-server/ecs.tf
@@ -79,6 +79,7 @@ resource "aws_ecs_task_definition" "service" {
       name : var.app_name,
       memory : 1024,
       memoryReservation : 512,
+      cpu: 512,
       // TODO: these are being passed in plaintext - refactor to use docker secrets or parameter store
       environment : [
         {
