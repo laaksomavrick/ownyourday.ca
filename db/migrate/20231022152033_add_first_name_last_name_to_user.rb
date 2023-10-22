d290a5a1e8c04c569c14e7cd13e6c9eba1765cf2
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-10-22 11:54:21 -0400

    Added initials to user navigation element

--- db/migrate/20231022152033_add_first_name_last_name_to_user.rb
+++ db/migrate/20231022152033_add_first_name_last_name_to_user.rb
@@ -2,7 +2,9 @@
 
 class AddFirstNameLastNameToUser < ActiveRecord::Migration[7.0]
   def change
+    # rubocop:disable Rails/BulkChangeTable
     add_column :users, :first_name, :string, null: false, default: ''
     add_column :users, :last_name, :string, null: false, default: ''
+    # rubocop:enable Rails/BulkChangeTable
   end
 end
