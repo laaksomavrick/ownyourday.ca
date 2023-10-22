b87a7a38a593187a98ad8ea7376b347910b97ec0
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-10-21 20:57:26 -0400

    Created /me route, page, and navigation element

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   resources :task_position, only: [:update]
   resources :goal_position, only: [:update]
 
+  get 'me', to: 'me#show'
+
   scope 'api' do
     get 'health_check', to: 'health_check#index'
   end
